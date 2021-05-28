56f56baaf1406a97facc320a341c115aba44725e
Ross Phillips <rphillips-nz@users.noreply.github.com>
2021-05-28 23:13:07 +1200

    Add cache

--- cli.js
+++ cli.js
@@ -1,13 +1,47 @@
 #!/usr/bin/env node
 
-const fs = require('fs')
+const fs = require('fs').promises;
 const runner = require('./runner')
+const { lookpath } = require('lookpath');
 
-const raw = fs.readFileSync('.pyper.json');
-const rules = JSON.parse(raw);
 
-const promises = Promise.all(rules.map(runner.processRule));
+(async () => {
+	let rules = {};
+	try {
+		const raw = await fs.readFile('.pyper.json');
+		rules = JSON.parse(raw);
+		console.log('Config found.');
+	} catch {
+		console.log('No config found.');
+		process.exit(1);
+	}
 
-promises
-	.then(() => console.log('Done.'))
-	.catch((err) => console.log('Failed', err));
+	let cache = {};
+	try {
+		const raw = await fs.readFile('.pyper/cache.json');
+		cache = JSON.parse(raw);
+		console.log('Cache found.');
+	} catch {
+		cache = {};
+		console.log('No cache found.');
+	}
+
+	const options = { cache };
+
+	options.md5Path = await lookpath('md5');
+	if (!options.md5Path) {
+		options.md5Path = await lookpath('md5sum');
+		if (!options.md5Path) {
+			console.error('Neither md5 or md5sum found in path');
+			process.exit(1);
+		}
+	}
+
+	console.log(`Using MD5 tool at ${options.md5Path}`);
+
+	const promises = Promise.all(rules.map((rule) => runner.processRule(rule, options)));
+
+	promises
+		.then(() => console.log('Done.'))
+		.catch((err) => console.log('Failed', err));
+})();
--- runner.js
+++ runner.js
@@ -2,6 +2,8 @@ const path = require('path');
 const sharp = require('sharp');
 const fs = require('fs').promises;
 const fg = require('fast-glob');
+const util = require('util');
+const execFile = util.promisify(require('child_process').execFile);
 
 function getPathParts(filePath) {
 	const extension = path.extname(filePath);
@@ -22,29 +24,58 @@ function getOutputPath(filePath, output) {
 		.replace(/\/+/, '/');
 }
 
-async function processFilePath(filePath, rule) {
-	const outputFilePath = getOutputPath(filePath, rule.output);
-	const convert = sharp(filePath);
+async function writeFile(filePath, data) {
+	await fs.mkdir(path.dirname(filePath), { recursive: true });
+	await fs.writeFile(filePath, data);
+}
 
-	if (rule.options?.format) {
-		convert.toFormat(rule.options?.format);
+async function getResizedBuffer(inputBuffer, output) {
+	const convert = sharp(inputBuffer);
+
+	if (output.options?.format) {
+		convert.toFormat(output.options?.format);
 	}
 
 	const { data } = await convert
-		.resize(rule.width, rule.height)
-		.jpeg({ quality: rule.options?.quality, force: false })
-		.png({ compressionLevel: rule.options?.compressionLevel, force: false })
+		.resize(output.width, output.height)
+		.jpeg({ quality: output.options?.quality, force: false })
+		.png({ compressionLevel: output.options?.compressionLevel, force: false })
 		.toBuffer({ resolveWithObject: true });
 
-	console.log(`From ${filePath} to ${outputFilePath}`);
-	await fs.mkdir(path.dirname(outputFilePath), { recursive: true });
-	await fs.writeFile(outputFilePath, data);
+	return data;
 }
 
-async function processRule(rule) {
-	const filePaths = await fg(rule.input);
-	console.log(`Found ${filePaths.length} path${filePaths.length === 1 ? '' : 's'} for ${rule.input}`);
-	await Promise.all(filePaths.map(async (filePath) => await processFilePath(filePath, rule)));
+async function processFilePath(filePath, output, cache, hash) {
+	const outputFilePath = getOutputPath(filePath, output.path);
+	const inputBuffer = await fs.readFile(filePath);
+	const cacheKey = `${filePath}|${outputFilePath}`;
+	const isCached = cache[cacheKey] === hash;
+
+	if (isCached) {
+		console.log(`Skipping ${filePath} to ${outputFilePath}`);
+	} else {
+		const data = await getResizedBuffer(inputBuffer, output);
+		console.log(`Resizing ${filePath} to ${outputFilePath}`);
+		await writeFile(outputFilePath, data);
+	}
+
+	cache[cacheKey] = hash;
+}
+
+async function processRule(rule, { cache, md5Path }) {
+	const paths = await fg(rule.input);
+	console.log(`Found ${paths.length} path${paths.length === 1 ? '' : 's'} for ${rule.input}`);
+
+	const allPromises = rule.outputs.reduce((memo, output) => {
+		return memo.concat(paths.map(async (filePath) => {
+			const { stdout } = await execFile(md5Path, [filePath]);
+			const hash = stdout.split(' = ')[1].trim();
+			await processFilePath(filePath, output, cache, hash);
+		}));
+	}, []);
+
+	await Promise.all(allPromises);
+	await writeFile('.pyper/cache.json', JSON.stringify(cache, null, 2));
 }
 
 module.exports = {
